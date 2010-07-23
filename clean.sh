3d8faf777a76133fcebb14be20d2767b8696d403
Jean Parpaillon <jean.parpaillon@kerlabs.com>
2010-07-23 16:40:15 +0200

    Initial autoconf stuff

---
+++ clean.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Copyright 2010 Kerlabs, All rights reserved
+#
+# Authors:
+#   Jean Parpaillon <jean.parpaillon@kerlabs.com>
+#
+set -e
+
+AUTOGEN_FILES="aclocal.m4 autom4te.cache configure config.guess config.log config.sub config.status depcomp compile libtool ltmain.sh missing mkinstalldirs config.h config.h.in m4/* py-compile"
+
+echo "Clean autogen generated files"
+for file in $AUTOGEN_FILES; do
+    ( cd $(dirname $0) && rm -rf $file )
+done
